e7df2425c3c104eda024fcf78a3bf4f8853c101d
mantonik <mantonik@gmail.com>
2024-11-30 11:58:50 -0500

    fix config name

--- bin/start_n8n.sh
+++ bin/start_n8n.sh
@@ -6,7 +6,7 @@ if [ $1"x" == "dx" ]; then
 fi 
 
 # Load enviremnet file 
-. ~/etc/n8n.conf
+. ~/etc/n8n.cfg
 
 #docker run -it --rm --name n8n -p 5678:5678 -v n8n_data:/home/node/.n8n docker.n8n.io/n8nio/n8n &
 
