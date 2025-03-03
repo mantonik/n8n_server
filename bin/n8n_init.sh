da3680851bff74ed07276d9224024faece4802de
Mariusz Antonik <mantonik@gmail.com>
2025-03-02 21:42:13 -0500

    update n8n init, add start of nginx and docker

--- bin/n8n_init.sh
+++ bin/n8n_init.sh
@@ -7,5 +7,13 @@ yum install -y docker nginx
 #Copy nginx config file 
 cp ../server-config/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 
-ls -lrt - /etc/nginx/nginx.conf
+#Start services 
+service nginx start 
+
+service docker start
+
+#Create n8n_data
+docker volume create n8n_data
+
+ls -lrt /etc/nginx/nginx.conf
 exit
