7c858bf9435c46ee8a1187193bfc166e524dd2c0
Mariusz Antonik <mantonik@gmail.com>
2025-03-30 20:19:04 -0400

    Add SSL certificate generation for nginx to get 443 port working

--- bin/n8n_init.sh
+++ bin/n8n_init.sh
@@ -8,6 +8,16 @@ dnf update
 
 yum install -y docker nginx policycoreutils-python-utils
 
+#Generate SSL certificate 
+mkdir -p /etc/nginx/ssl
+
+sudo openssl req -x509 -nodes -days 365 \
+  -newkey rsa:2048 \
+  -keyout /etc/nginx/ssl/selfsigned.key \
+  -out /etc/nginx/ssl/selfsigned.crt \
+  -subj "/C=US/ST=State/L=City/O=Organization/OU=IT/CN=localhost"
+
+  
 #Copy nginx config file 
 cp ../server-config/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 
