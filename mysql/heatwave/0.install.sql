7978e86560c8fc2201e03d0f1531df929a531d72
Mariusz Antonik <mantonik@gmail.com>
2025-06-28 22:03:49 -0400

    add fix for patch 3 - patch 4

--- mysql/heatwave/0.install.sql
+++ mysql/heatwave/0.install.sql
@@ -1,3 +1,4 @@
+SET NAMES utf8mb4;
 SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_01.sql
 SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_patch_01.sql
 SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_patch_02.sql
