f7fe4bc03cc6a4e52205f632ffc158c7e0d94e33
Mariusz Antonik <mantonik@gmail.com>
2025-06-28 21:51:51 -0400

    add heatwave patch 02

---
+++ mysql/heatwave/0.install.sql
@@ -0,0 +1,3 @@
+SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_01.sql
+SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_patch_01.sql
+SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_patch_02.sql
---
+++ mysql/heatwave/heatwave_monitoring_patch_01.sql
@@ -0,0 +1,8 @@
+-rw-r-----. 1 root root 32483 Jun 28 19:09 heatwave_monitoring_01.sql
+-rw-r-----. 1 root root  8165 Jun 28 19:09 heatwave_monitoring_guide.html
+-rw-r-----. 1 root root     0 Jun 28 19:09 heatwave_monitoring_patch_01.sql
+
+
+SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_01.sql
+SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_patch_01.sql
+
