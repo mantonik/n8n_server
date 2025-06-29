86e444d4af52846610798bafda124b98779d67a0
Mariusz Antonik <mantonik@gmail.com>
2025-06-28 21:56:45 -0400

    add patch 3 for heatwave

--- mysql/heatwave/0.install.sql
+++ mysql/heatwave/0.install.sql
@@ -1,3 +1,4 @@
 SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_01.sql
 SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_patch_01.sql
 SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_patch_02.sql
+SOURCE /root/bin/n8n_server/mysql/heatwave/heatwave_monitoring_patch_03.sql
